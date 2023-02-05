217062ac0bb3cd4694235aa4adf8cb6a862e3419
Michaela Chang <michaela.w.chang@gmail.com>
2023-02-04 23:41:44 -0800

    App works to send SMS request!

--- frontend/hacksc_23/lib/main.dart
+++ frontend/hacksc_23/lib/main.dart
@@ -1,115 +1,225 @@
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_sms/flutter_sms.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+const List<String> phoneNumber = ['8447443520'];
+const double _kItemExtent = 30.0;
+const List<String> _transType = <String>[
+  'Driving',
+  'Walking',
+  'Bicycling',
+  'Transit'
+];
+const Color mainColor = Color.fromARGB(255, 122, 102, 255);
+const Color tintedWhite = Color.fromARGB(16, 0, 0, 0);
+void main() => runApp(const App());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const CupertinoApp(
+      theme: CupertinoThemeData(
+          brightness: Brightness.light, primaryColor: mainColor),
+      home: Wrapper(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Wrapper extends StatefulWidget {
+  const Wrapper({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Wrapper> createState() => _WrapperState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _WrapperState extends State<Wrapper> {
+  int _selectedTransType = 0;
+  final _formKey = GlobalKey<FormState>();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  TextEditingController startPointController = TextEditingController();
+  TextEditingController destinationController = TextEditingController();
+
+  Future<void> _pushToText(String message, List<String> recipients) async {
+    try {
+      if (await Permission.sms.request().isGranted) {
+        String _result = await sendSMS(
+          message: message,
+          recipients: recipients,
+          sendDirect: true,
+        );
+      }
+    } catch (error) {
+      print(error);
+    }
+  }
+
+  void _showDialog(Widget child) {
+    showCupertinoModalPopup<void>(
+        context: context,
+        builder: (BuildContext context) => Container(
+              height: 150.0,
+              padding: const EdgeInsets.only(top: 6.0),
+              // The Bottom margin is provided to align the popup above the system navigation bar.
+              margin: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom,
+              ),
+              // Provide a background color for the popup.
+              color: CupertinoColors.systemBackground.resolveFrom(context),
+              // Use a SafeArea widget to avoid system overlaps.
+              child: SafeArea(
+                top: false,
+                child: child,
+              ),
+            ));
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return CupertinoPageScaffold(
+      child: Center(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const SizedBox(height: 75),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                          color: tintedWhite,
+                          borderRadius: BorderRadius.circular(10)),
+                      padding: const EdgeInsets.all(10),
+                      child: Column(
+                        children: <Widget>[
+                          /* STARTING POINT */
+                          CupertinoTextField.borderless(
+                            placeholder: 'Starting Point',
+                            controller: startPointController,
+                            prefix: const Icon(
+                                IconData(0xe061, fontFamily: 'MaterialIcons')),
+                          ),
+                          const Divider(
+                            thickness: 1,
+                            color: Colors.black12,
+                          ),
+
+                          /* DESTINATION */
+                          CupertinoTextField.borderless(
+                            placeholder: 'Destination',
+                            controller: destinationController,
+                            prefix: const Icon(
+                                IconData(0xe3ab, fontFamily: 'MaterialIcons')),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        /* TRANSPORTATION METHOD */
+                        CupertinoButton(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 0,
+                            horizontal: 0,
+                          ),
+                          // Display a CupertinoPicker with list of transportation types.
+                          onPressed: () => _showDialog(
+                            CupertinoPicker(
+                              magnification: 1.0,
+                              squeeze: 1.0,
+                              useMagnifier: true,
+                              itemExtent: _kItemExtent,
+                              // This is called when selected item is changed.
+                              onSelectedItemChanged: (int selectedItem) {
+                                setState(() {
+                                  _selectedTransType = selectedItem;
+                                });
+                              },
+                              children: List<Widget>.generate(_transType.length,
+                                  (int index) {
+                                return Center(
+                                  child: Text(
+                                    _transType[index],
+                                  ),
+                                );
+                              }),
+                            ),
+                          ),
+                          // This displays the selected destination name.
+                          // prefixIcon: Icon(
+                          //   IconData(0xe1d5, fontFamily: 'MaterialIcons'),
+                          // ),
+                          alignment: Alignment.centerLeft,
+                          child: RichText(
+                            text: TextSpan(
+                              children: [
+                                const WidgetSpan(
+                                    child: Padding(
+                                  padding:
+                                      EdgeInsets.symmetric(horizontal: 3.0),
+                                  child: Icon(
+                                    IconData(0xe1d5,
+                                        fontFamily: 'MaterialIcons'),
+                                  ),
+                                )),
+                                TextSpan(
+                                  text: _transType[_selectedTransType],
+                                  style: const TextStyle(
+                                    color: mainColor,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                  ],
+                ),
+              ),
+              /* NAVIGATION DIRECTIONS BOX */
+              Expanded(
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: tintedWhite,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 25),
+
+              /* SUBMIT DIRECTIONS FORM */
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CupertinoButton.filled(
+                    onPressed: () {
+                      // BUILD JSON
+                      String json =
+                          '{"start": "${startPointController.text}", "destination": "${destinationController.text}", "mode": "${_transType[_selectedTransType].toLowerCase()}"}';
+
+                      _pushToText(json, phoneNumber);
+                    },
+                    child: const Text('Find Directions'),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 15),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
